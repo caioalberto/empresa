6456d8eaac9c92399593ec3eeed9612c2b7c43e0
Caio Ribeiro <Caio Ribeiro@192.168.0.56>
2016-02-03 16:09:06 -0200

    Criação

---
+++ src/main/java/br/com/caioribeiro/empresa/Empresa.java
@@ -0,0 +1,5 @@
+package br.com.caioribeiro.empresa;
+
+public class Empresa {
+
+}
---
+++ src/test/java/br/com/caioribeiro/empresa/EmpresaTest.java
@@ -0,0 +1,14 @@
+package br.com.caioribeiro.empresa;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EmpresaTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
