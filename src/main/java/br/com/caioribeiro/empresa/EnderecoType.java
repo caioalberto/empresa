85c7691050a55e7bf8c381a9a7f2d09f337cc1c2
Caio Ribeiro <Caio Ribeiro@192.168.0.56>
2016-03-04 13:41:14 -0300

    Comentarios

--- src/main/java/br/com/caioribeiro/empresa/EnderecoType.java
+++ src/main/java/br/com/caioribeiro/empresa/EnderecoType.java
@@ -2,17 +2,37 @@ package br.com.caioribeiro.empresa;
 
 
 
+/**
+ * The Enum EnderecoType.
+ */
+/**
+ * @author Caio Ribeiro
+ *
+ */
 public enum EnderecoType {
     
-    COMERCIAL("Comcercial"), RESIDENCIAL ("Residencial");
+    /** The comercial. */
+    COMERCIAL("Comcercial"), /** The residencial. */
+ RESIDENCIAL ("Residencial");
     
+    /** The nome tipo endereco. */
     private final String nomeTipoEndereco;
     
+    /**
+     * Cria uma instancia de endereco type.
+     *
+     * @param descricao the descricao
+     */
     EnderecoType(String descricao){
         nomeTipoEndereco = descricao;
     }
     
 
+    /**
+     * Gets the nome tipo endereco.
+     *
+     * @return the nome tipo endereco
+     */
     public String getNomeTipoEndereco() {
         return nomeTipoEndereco;
     }
--- src/main/java/br/com/caioribeiro/empresa/TelefoneType.java
+++ src/main/java/br/com/caioribeiro/empresa/TelefoneType.java
@@ -3,24 +3,55 @@ package br.com.caioribeiro.empresa;
 import org.hibernate.validator.constraints.NotBlank;
 import org.hibernate.validator.constraints.Range;
 
+/**
+ * The Enum TelefoneType.
+ */
+/**
+ * @author Caio Ribeiro
+ *
+ */
 public enum TelefoneType {
-    CELULAR(9, "Celular"), FAX(8, "Fax"), COMERCIAL(8, "Comercial");
+    
+    /** The celular. */
+    CELULAR(9, "Celular"), 
+ /** The fax. */
+ FAX(8, "Fax"), 
+ /** The comercial. */
+ COMERCIAL(8, "Comercial");
    
+   /** The qtd numeros. */
    @Range(min=8, max=9, message="Número de telefone inválido, insira um telefone com a qunatidade de números entre {min} e {max}!") 
    private final int qtdNumeros;
    
+   /** The nome tipo. */
    @NotBlank(message="O tipo não pode estar vazio!")
    private final String nomeTipo;
    
+    /**
+     * Cria uma instancia de telefone type.
+     *
+     * @param telefone the telefone
+     * @param descricao the descricao
+     */
     TelefoneType(int telefone, String descricao){
         qtdNumeros = telefone;
         nomeTipo = descricao;
     }
 
+    /**
+     * Gets the qtd numeros.
+     *
+     * @return the qtd numeros
+     */
     public int getQtdNumeros() {
         return qtdNumeros;
     }
 
+    /**
+     * Gets the nome tipo.
+     *
+     * @return the nome tipo
+     */
     public String getNomeTipo() {
         return nomeTipo;
     }
